e8fbbef4a4ba864a7131224980fbe111f358b62e
Nishanth <agent006nishanth@gmail.com>
2024-06-15 12:24:54 +0530

    setup project files - part 1

--- src/utils/constants.mjs
+++
@@ -1,5 +0,0 @@
-export const mockUsers=[
-    {id:1, username:"nishanth",displayName:"nishi"},
-    {id:2, username:"sandeep",displayName:"arju"},
-    {id:3, username:"lalitha",displayName:"lali"},
-]
